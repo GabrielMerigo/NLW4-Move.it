91f4d5d567bef98651f618bb890a96b138fe5640
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-02-25 22:50:25 -0300

    new Files

--- src/Components/Countdown.tsx
+++ src/Components/Countdown.tsx
@@ -2,23 +2,38 @@ import { useState } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown(){
-    const [] = useState();
+    const [time, setTime] = useState(25 * 60)
+    const [active, setActive] = useState()
+
+    const minutes = Math.floor(time / 60)
+    const second = time % 60
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+    const [secondLetf, secondRight] = String(second).padStart(2, '0').split('')
+
+    function startCountdown(){
+
+    }
 
     return(
         <div>
 
         <div className={styles.countdownContainer}>
             <div>
-                <span>2</span>
-                <span>5</span>
+                <span>{minuteLeft}</span>
+                <span>{minuteRight}</span>
             </div>
             <span>:</span>
             <div>
-                <span>0</span>    
-                <span>0</span>    
+                <span>{secondLetf}</span>    
+                <span>{secondRight}</span>    
             </div>    
         </div>
-        <button type="button" className={styles.countdownButton}>
+        <button 
+        type="button" 
+        className={styles.countdownButton}
+        onClick={startCountdown}
+        >
             Iniciar Ciclo
         </button>
         </div>
