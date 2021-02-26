d37ce0e3b5cbb7155f5bae5441e730d2bf23f159
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-02-25 22:33:41 -0300

    new Features

--- src/Components/Countdown.tsx
+++ src/Components/Countdown.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown(){
+    const [] = useState();
+
     return(
+        <div>
+
         <div className={styles.countdownContainer}>
             <div>
                 <span>2</span>
                 <span>5</span>
             </div>
-                <span>:</span>
+            <span>:</span>
             <div>
                 <span>0</span>    
                 <span>0</span>    
             </div>    
+        </div>
+        <button type="button" className={styles.countdownButton}>
+            Iniciar Ciclo
+        </button>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <title>Início | movi.it</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/png"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
       </Head>
